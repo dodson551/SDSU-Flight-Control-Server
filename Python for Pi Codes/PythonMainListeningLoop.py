0e9ece1318f937640a5c1d01404dbf51f36a1b79
Jacob Dodson <jacobtdodson@gmail.com>
2016-08-12 12:23:24 -0700

    added exit thread to listen for exit command from GUI on separate socket. Also added socket reuse flag to allow for reuse of sockets when program exits.

--- Python for Pi Codes/PythonMainListeningLoop.py
+++ Python for Pi Codes/PythonMainListeningLoop.py
@@ -6,15 +6,32 @@ import serial
 import thread
 import threading
 
+# global variable to check if GUI requests exit from program
+exitReq = False
+
+# buffer size global variable 
+BUF = 4096
+
 # create a serial connection to arduino
 ser = serial.Serial('/dev/ttyACM0', 9600)
 
-# create UDP socket
+# socket setup
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR,1)
+
+exitsock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+exitsock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR,1)
+
+# create UDP socket for listener
 hostIp = "192.168.1.227"
 port = 6969
 serverAddress = (hostIp, port)
 sock.bind(serverAddress)
+
+# create seperate UDP socket for exit thread
+exitport = 7979
+exitserverAddress = (hostIp, exitport)
+exitsock.bind(exitserverAddress)
 
 print "Socket setup: %s | %s" % serverAddress
 print "Listening..."
@@ -24,47 +41,59 @@ mostRecentX = 0.0;
 mostRecentY = 0.0;
 
 def MAIN():
+	
+	exit_trd = threading.Thread(target=exit_thread())
+	exit_trd.start()
+
 	try:
 
-		
 		# main listening loop
 		while True:
-			data, address = sock.recvfrom(4096)
-			
-			# when a UDP packet arrives...
-			if data:
-				# convert received bytes to string
-				decodedStr = data.decode("utf-8")
+			if exitReq is True:
+				print "Exiting listening loop."
+				print "\nClosing serial connection..."
+				ser.close()
+				print "Closing socket..."
+				sock.close()
+				print "\n"
+				sys.exit()
+			else:
+				data, address = sock.recvfrom(BUF)
 				
-				print "Message Received: %s" % decodedStr
-
-				if 's' in decodedStr:
-					output = update_settings(decodedStr)
-					print output
-
-				
-				# # use string to find the function to callable
-				# if decodedStr == "Start X Pattern":
-				# 	print "Calling XPattern..."
-				# 	sock.sendto("Starting X Pattern", address)
+				# when a UDP packet arrives...
+				if data:
+					# convert received bytes to string
+					decodedStr = data.decode("utf-8")
 					
-				# 	# start X Pattern
-				# 	try:
-				# 		thread.start_new_thread( StartPattern, ('1', ser, ) )
-				# 	except: 
-				# 		print "Error Starting Thread..."
+					print "Message Received: %s" % decodedStr
+
+					if 's' in decodedStr:
+						output = update_settings(decodedStr)
+						print output
+
+					
+					# # use string to find the function to callable
+					# if decodedStr == "Start X Pattern":
+					# 	print "Calling XPattern..."
+					# 	sock.sendto("Starting X Pattern", address)
 						
-				# elif decodedStr == "Get Values":
-				# 	msg = FormatXYValuesMessage(mostRecentX, mostRecentY)
-				# 	sent = sock.sendto(msg, address)
-				
-				# else: 
-				# 	msg = "Incorrect Command.  Try Again..."
-				# 	sent = sock.sendto(msg, address)
+					# 	# start X Pattern
+					# 	try:
+					# 		thread.start_new_thread( StartPattern, ('1', ser, ) )
+					# 	except: 
+					# 		print "Error Starting Thread..."
+							
+					# elif decodedStr == "Get Values":
+					# 	msg = FormatXYValuesMessage(mostRecentX, mostRecentY)
+					# 	sent = sock.sendto(msg, address)
 					
-				# # when command has been executed, update console...
-				# print "Waiting for Command..."
-				
+					# else: 
+					# 	msg = "Incorrect Command.  Try Again..."
+					# 	sent = sock.sendto(msg, address)
+						
+					# # when command has been executed, update console...
+					# print "Waiting for Command..."
+					
 	except KeyboardInterrupt:
 		print "\nClosing serial connection..."
 		ser.close()
@@ -79,6 +108,16 @@ def update_settings(string):
 	ser.write(string)
 	outputStr = ser.readline()
 	return outputStr
+
+def exit_thread():
+	while True:
+		data, address = exitsock.recvfrom(BUF)
+		if data:
+			if 'exit' in data:
+				exitReq = True
+			else:
+				exitReq = False
+		return
 
 def StartPattern(pattern, ser):
 	global mostRecentX
